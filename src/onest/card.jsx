a9be862205b315e0248135edd13bd029e26db6f2
nawaz shaik <shariefnawaz1995@gmail.com>
2024-06-08 13:59:07 +0530

    Removed Chakra UI from Landing Page, List Page and Details Pages

--- src/onest/card.jsx
+++ src/onest/card.jsx
@@ -1,3 +1,4 @@
+import "./styles.css";
 const env = import.meta.env;
 
 export const dataConfig = {
@@ -17,6 +18,71 @@ export const dataConfig = {
     imageUrl: "",
 
     apiResponse: (e) => e.data?.data?.[env.VITE_SCHOLASHIPS_DB_CACHE],
+    onOrderIdGenerate: async (val) => {
+      const data = {
+        user_id: `${val.userData.user_id}`,
+        context: val.type,
+        context_item_id: val.jobId,
+        status: "created",
+        order_id:
+          val.response?.data?.data[process.env.VITE_SCHOLARSHIPS_INSERT_ORDER]
+            ?.returning?.[0]?.order_id,
+      };
+      // You can track this data by API call
+      console.log("data", data);
+    },
+    render: (obj) => {
+      const getDates = (range) => {
+        const formatDate = (date) => {
+          const options = { day: "2-digit", month: "short", year: "numeric" };
+          return new Date(date).toLocaleDateString("en-GB", options);
+        };
+        return `${formatDate(range.start)} to ${formatDate(range.end)}`;
+      };
+
+      const getMinEligibilityValue = (data) => {
+        const academicEligibility = data.find(
+          (item) => item.descriptor.code === "academic-eligibility"
+        );
+        if (academicEligibility && academicEligibility.list.length > 0) {
+          const minEligibility = academicEligibility?.list
+            ?.map((item) => item.value)
+            .join(", ");
+          return minEligibility;
+        }
+        return "-";
+      };
+      return (
+        <div className="container">
+          {obj?.image_url && (
+            <img className="image" src={obj.image_url} alt="no IMAGE" />
+          )}
+          <div className="title">{obj?.title}</div>
+          <div className="row">
+            <i className="icon">🏢</i> {/* Replace with appropriate icon */}
+            <div className="text">
+              {obj?.descriptor?.name
+                ? obj?.descriptor?.name
+                : "Provider name not mentioned"}
+            </div>
+          </div>
+          <div className="row">
+            <i className="icon">💼</i> {/* Replace with appropriate icon */}
+            <div className="text">{getMinEligibilityValue(obj.item.tags)}</div>
+          </div>
+          <div className="row">
+            <i className="icon">📅</i> {/* Replace with appropriate icon */}
+            <div className="text">{getDates(obj.item.time.range)}</div>
+          </div>
+          <div className="row">
+            <div className="price-symbol">₹</div>
+            <div className="price">
+              {obj.item?.price?.value ? obj.item.price.value : "-"}
+            </div>
+          </div>
+        </div>
+      );
+    },
   },
 
   jobs: {
@@ -45,15 +111,68 @@ export const dataConfig = {
     imageUrl: "",
 
     apiResponse: (e) => e.data?.data?.[env.VITE_JOBS_DB_CACHE],
-    // render: (e) => {
-    //   console.log(e);
-    //   return (
-    //     <div>
-    //       <h1>{e.title} </h1>
-    //       <h2>{e.company}</h2>
-    //     </div>
-    //   );
-    // },
+    onOrderIdGenerate: async (val) => {
+      const data = {
+        user_id: val?.userData.user_id,
+        context: val?.type,
+        context_item_id: val?.jobId,
+        status: "created",
+        order_id:
+          val?.response?.data?.data[process.env.VITE_JOBS_INSERT_ORDER]
+            ?.returning?.[0]?.order_id,
+        provider_name: val?.item?.provider_name || "",
+        item_name: val?.item?.title || "",
+      };
+      // You can track this data by API call
+      console.log("data", data);
+    },
+    render: (obj) => {
+      console.log("obj", obj);
+      const getSalaryInfo = (data) => {
+        // Find the object with descriptor.code === "salary-info"
+        const salaryInfo = data.find(
+          (item) => item.descriptor.code === "salary-info"
+        );
+        // If the object is found, proceed to return the values of list[0] and list[1]
+        if (salaryInfo && salaryInfo.list.length >= 2) {
+          const minSalary = salaryInfo.list[0].value;
+          const maxSalary = salaryInfo.list[1].value;
+          return `Rs. ${minSalary} - Rs. ${maxSalary}`;
+        }
+        // Return null if the object or required list items are not found
+        return "As per Industry Standards";
+      };
+      return (
+        <div className="container">
+          {obj?.image_url && (
+            <img className="image" src={obj.image_url} alt="no IMAGE" />
+          )}
+          <div className="title">{obj?.title}</div>
+          <div className="row">
+            <i className="icon">🏢</i> {/* Replace with appropriate icon */}
+            <div className="text">
+              {obj?.item?.creator?.descriptor?.name
+                ? obj?.item?.creator?.descriptor?.name
+                : "Company name not mentioned"}
+            </div>
+          </div>
+          <div className="row">
+            <i className="icon">📍</i> {/* Replace with appropriate icon */}
+            <div className="text">{`${obj.city}, ${obj.state}`}</div>
+          </div>
+          <div className="row">
+            <i className="icon">💼</i> {/* Replace with appropriate icon */}
+            <div className="text">
+              {obj.fulfillments ? obj.fulfillments : "-"}
+            </div>
+          </div>
+          <div className="row">
+            <div className="price-symbol">₹</div>
+            <div className="price">{getSalaryInfo(obj.item.tags)}</div>
+          </div>
+        </div>
+      );
+    },
   },
   learning: {
     title: "Learning Experiences",
@@ -71,42 +190,63 @@ export const dataConfig = {
     apiLink_BASE_URL: env.VITE_BASE_URL,
 
     apiResponse: (e) => e.data?.data?.[env.VITE_LEARNINGS_DB_CACHE],
-    // apiResponse: ({ data }) => {
-    //   let response = [];
-    //   //   response = data?.message?.catalog?.providers?.flatMap((e) => e.items);
-    //   return data.data;
-    // },
-    // render: (e) => {},
-    payload: {
-      context: {
-        domain: "onest:learning-experiences",
-        action: "search",
-        version: "1.1.0",
-        bap_id: "13.201.4.186:6002",
-        bap_uri: "http://13.201.4.186:6002/",
-        location: {
-          country: {
-            name: "India",
-            code: "IND",
-          },
-          city: {
-            name: "Bangalore",
-            code: "std:080",
-          },
-        },
-        transaction_id: "a9aaecca-10b7-4d19-b640-b047a7c60008",
-        message_id: "a9aaecca-10b7-4d19-b640-b047a7c60009",
-        timestamp: "2023-02-06T09:55:41.161Z",
-      },
-      message: {
-        intent: {
-          item: {
-            descriptor: {
-              name: "",
-            },
-          },
-        },
-      },
+
+    onOrderIdGenerate: async (val) => {
+      const paramData = { url: "", type: "" };
+      paramData.url =
+        val.response.responses?.[0]?.message.order?.fulfillments?.[0]?.stops?.[0]?.instructions?.media?.[0]?.url;
+      paramData.type =
+        val.response.responses?.[0]?.message.order?.fulfillments?.[0]?.stops?.[0]?.type;
+
+      const data = {
+        user_id: `${val.userData.user_id}`,
+        context: val.type,
+        context_item_id: val.itemId,
+        status: "created",
+        order_id: val.response.responses?.[0]?.message.order.id,
+        provider_name: val?.item?.provider_name || "",
+        item_name: val?.item?.title || "",
+        params: JSON.stringify(paramData),
+      };
+      // You can track this data by API call
+      console.log("data", data);
+    },
+    render: (obj) => {
+      return (
+        <div
+          className="container"
+          style={{ display: "flex", flexDirection: "row" }}
+        >
+          <div>
+            {obj?.image_url && (
+              <img className="image" src={obj.image_url} alt="no IMAGE" />
+            )}
+          </div>
+          <div>
+            <div className="title">{obj?.title}</div>
+            <div className="row">
+              <strong>Published By:</strong>
+              <div className="text">
+                {obj?.provider_name
+                  ? obj?.provider_name
+                  : "Provider name not mentioned"}
+              </div>
+            </div>
+            <div className="row">
+              <strong>Description:</strong>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: obj.shortDescription
+                    ? obj.shortDescription
+                    : obj.description
+                    ? obj.description.substring(0, 100) + "..."
+                    : "",
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      );
     },
   },
 };
