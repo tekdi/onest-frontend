c42e2a2e86cc8cf61806009406d4b64d9a291fdd
nawaz shaik <shariefnawaz1995@gmail.com>
2024-06-11 12:59:06 +0530

    Page limit and numbers function updated

--- src/onest/Pagination.jsx
+++ src/onest/Pagination.jsx
@@ -12,7 +12,23 @@ const Pagination = ({ limit, page, totalRows, onPageChange }) => {
 
   const renderPageNumbers = () => {
     const pageNumbers = [];
-    for (let i = 1; i <= totalPages; i++) {
+    let startPage, endPage;
+    if (totalPages <= 10) {
+      startPage = 1;
+      endPage = totalPages;
+    } else {
+      if (page <= 6) {
+        startPage = 1;
+        endPage = 10;
+      } else if (page + 4 >= totalPages) {
+        startPage = totalPages - 9;
+        endPage = totalPages;
+      } else {
+        startPage = page - 5;
+        endPage = page + 4;
+      }
+    }
+    for (let i = startPage; i <= endPage; i++) {
       pageNumbers.push(
         <button
           key={i}
